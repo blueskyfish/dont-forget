f5a3525e5516ab52c6da32586fe478407a719548
BlueSkyFish <blueskyfish@blueskyfish.de>
2020-08-07 14:56:35 +0200

    Backend: define datascheme

--- backend/data/01-create-tables.sql
+++ backend/data/01-create-tables.sql
@@ -31,3 +31,53 @@ CREATE TABLE IF NOT EXISTS settings (
   type TEXT NOT NULL,
   description TEXT NOT NULL
 );
+
+
+--
+-- groups
+--
+
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE IF NOT EXISTS groups (
+  group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  color TEXT NOT NULL DEFAULT '#ffff99',
+  icon TEXT NOT NULL DEFAULT 'note-outline',
+  owner_id INTEGER
+);
+
+--
+-- Members
+--
+
+DROP TABLE IF EXISTS members;
+
+CREATE TABLE IF NOT EXISTS members (
+  member_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  enable INTEGER NOT NULL DEFAULT 1
+);
+
+DROP INDEX IF EXISTS uni_group_members;
+CREATE UNIQUE INDEX IF NOT EXISTS uni_group_members ON members (group_id, user_id);
+
+
+--
+-- Notes
+--
+
+DROP TABLE IF EXISTS nodes;
+
+CREATE TABLE IF NOT EXISTS notes (
+  node_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  text TEXT DEFAULT NULL,
+  state TEXT NOT NULL DEFAULT 'open'
+);
+
+DROP INDEX IF EXISTS idx_node_states;
+CREATE INDEX IF NOT EXISTS idx_node_states ON "notes" ( "state" ASC );
--- backend/data/02-insert-masterdata.sql
+++ backend/data/02-insert-masterdata.sql
@@ -11,6 +11,27 @@ VALUES ('Susi', '-test1234', 'susi@test.de', '["admin", "user"]');
 INSERT INTO users (name, password, email, roles)
 VALUES ('Berti', '-test1234', 'berti@test.de', '["user"]');
 
+--
+-- Groups
+--
+INSERT INTO groups (title, owner_id)
+VALUES('Test Block', 1);
+
+--
+-- Members
+--
+INSERT INTO members (group_id, user_id)
+VALUES (1, 1);
+INSERT INTO members (group_id, user_id)
+VALUES (1, 2);
+
+--
+-- Notes
+--
+INSERT INTO notes (group_id, user_id, title, text)
+VALUES (1, 2, 'Test Notes', null);
+INSERT INTO notes (group_id, user_id, title, text)
+VALUES (1, 1, 'Einkaufen', 'Test');
 
 --
 -- Setting
